3d1866e4d2e9e8a3419a9fadacc259733dc0e4e6
Auralcat <ashleyzt@hi2.in>
2018-09-17 16:48:52 -0300

    Add user routing resource, create show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  # Set root to static_pages/home
+  root 'static_pages#home'
+
   get '/about',    to: 'static_pages#about'
   get '/contact',  to: 'static_pages#contact'
   get '/help',     to: 'static_pages#help'
-
   get '/signup',      to: 'users#new'
-  # Set root to static_pages/home
-  root 'static_pages#home'
+
+  # Resources route
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
