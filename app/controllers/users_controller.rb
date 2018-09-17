f2a29ef7ac1eed79eedc450585b73622602e273b
Auralcat <ashleyzt@hi2.in>
2018-09-17 16:58:45 -0300

    Add Byebug call inside show method in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # You can call Byebug inside the view, it opens in the server's console
+    # and halts the loading of the page until you press C-d.
+    # debugger
   end
 end
