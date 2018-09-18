9e6fda24ae6afe90a372d1272dd17416b6037b88
Auralcat <ashleyzt@hi2.in>
2018-09-17 22:31:00 -0300

    Make it so user returns to signup_path on error

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # Set root to static_pages/home
   root 'static_pages#home'
 
-  get '/about',    to: 'static_pages#about'
-  get '/contact',  to: 'static_pages#contact'
-  get '/help',     to: 'static_pages#help'
-  get '/signup',      to: 'users#new'
+  get   '/about',    to:  'static_pages#about'
+  get   '/contact',  to:  'static_pages#contact'
+  get   '/help',     to:  'static_pages#help'
+  get   '/signup',   to:  'users#new'
+  post  '/signup',   to:  'users#create'
 
   # Resources route
   resources :users
