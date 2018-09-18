01945b7ddeabe296b620224761c19bb5e9737eae
Auralcat <ashleyzt@hi2.in>
2018-09-17 22:16:40 -0300

    Write one more integration test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,4 +10,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: 'bar' } }
     end
   end
+
+  test 'page has inputs and labels for each field in the model' do
+    get signup_path
+
+    model_params = %w[name email password password_confirmation]
+    model_params.each do |param|
+      assert_select "#user_#{param}"
+      assert_select "label[for=user_#{param}]", param.humanize
+    end
+  end
+
+  test 'shows 5 error messages when receiving blank input' do
+    skip
+    get signup_path
+    assert_select 'ul' do
+      assert_select 'li', 5
+    end
+  end
 end
