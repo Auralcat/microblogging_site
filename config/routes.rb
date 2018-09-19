f1c8e63ca40ad3313ee426ec7a8162dc0cb9b637
Auralcat <ashleyzt@hi2.in>
2018-09-19 12:08:35 -0300

    Lay routes for Sessions controller, write first test

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   get   '/signup',   to:  'users#new'
   post  '/signup',   to:  'users#create'
 
+  # Login routes
+  get     '/login',   to:  'sessions#new'
+  post    '/login',   to:  'sessions#create'
+  delete  '/logout',  to:  'sessions#destroy'
+
   # Resources route
   resources :users
 
