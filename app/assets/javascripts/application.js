0cfefc9d21a78dc141970b39092c6c561133871d
Auralcat <ashleyzt@hi2.in>
2018-09-19 14:58:36 -0300

    Create navbar content when user is logged in, add Bootstrap's JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// Add Bootstrap JS component here.
+//= require bootstrap
+//=
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
