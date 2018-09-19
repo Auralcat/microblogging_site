3b05e76fc3c6ea269cdc5dbf48c1d9bcfb402e71
Auralcat <ashleyzt@hi2.in>
2018-09-19 12:27:42 -0300

    Implement session creation method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # user is logged in
+    else
+      # Create an error message
+      flash[:danger] = 'Invalid email/password combination.'
+      render 'new'
+    end
   end
 
   def destroy
