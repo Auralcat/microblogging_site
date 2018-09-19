929f7f2858dbeb46e2d5b063e63683fdc213fabc
Auralcat <ashleyzt@hi2.in>
2018-09-19 12:42:13 -0300

    Create cookie using log_in method in SessionsHelper module

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # user is logged in
+      log_in user
+      redirect_to user
     else
       # Create an error message
       flash.now[:danger] = 'Invalid email/password combination.'
