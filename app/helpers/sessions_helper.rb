098e244ecbbfae95b974d2429e575a60dc98e6a4
Auralcat <ashleyzt@hi2.in>
2018-09-21 11:27:44 -0300

    Add more debug info, reset session instead of deleting it

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,10 +2,11 @@ module SessionsHelper
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
+    puts "Session created: #{session}"
   end
 
   def log_out
-    session.delete(:user_id)
+    reset_session
     @current_user = nil
   end
 
